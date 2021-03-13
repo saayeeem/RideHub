f9319d0ac38c25040c9b068f0fd894d3ec69fa1a
= <chysayem9@gmail.com>
2021-03-13 16:25:52 +0600

    Update home.php

--- View/home.php
+++ View/home.php
@@ -2,22 +2,32 @@
 
 <body>
 
-    <h1><b>
-            <center> Welcome to RideHub </center>
-        </b> </h1>
-    <br>
-    <br>
-    <img src="Pictures/home.jpg" alt="Home" class="center">
-    <br>
-    <br>
-    <br>
-
-    Returning User? <a href="login.php">Login Now!!</a>
-    <br>
-    <br>
-    <br>
-
-    New User? <a href="RegisterHome.php">Explore Our World!!</a>
 </body>
 
 </html>
+
+<?php
+include('../control/ValidationLogin.php');
+
+if (!isset($_SESSION['email'])) {
+
+    echo "<h1><b>
+            <center> Welcome to RideHub </center>
+        </b> </h1>";
+    echo "  <br>
+    <br>";
+    echo '<img src="Pictures/home.jpg" alt="Home" class="center">';
+    echo "<br>
+    <br>
+    <br>";
+
+    echo 'Returning User? <a href="login.php">Login Now!!</a>';
+    echo "<br>
+    <br>
+    <br>";
+
+    echo 'New User? <a href="RegisterHome.php">Explore Our World!!</a>';
+} else if (isset($_SESSION['email'])) {
+    echo "Hi Bye";
+}
+?>
