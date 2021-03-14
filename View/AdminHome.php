ac414f85cb84973409874f3dd2939631b2d24b14
greencyber18 <39981392+greencyber18@users.noreply.github.com>
2021-03-14 14:14:30 +0600

    Admin

---
+++ View/AdminHome.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <nav>
+        <a href="AdminHome.php">Home</a> |
+
+        <a href="logout.php">Log Out</a>
+    </nav>
+
+    <p><img src="Pictures/Admin.jpg" alt="Home"></p>
+    <a href="DriverProfile.php">Registered Driver</a>
+    <br>
+    <br>
+
+    <a href="Showcar.php">Registered Cars</a>
+    <br>
+    <br>
+  <a href="AllVendor.php">Registered Vendors</a>
+  <br>
+  <br>
+<a href="AllUser.php">Registered Customers</a>
+
+</body>
+
+</html>
