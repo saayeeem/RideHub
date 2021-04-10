9d0edaf27e34bfc40742c2fb3f514aacfc029fe7
tausif-fardin <58599676+tausif-fardin@users.noreply.github.com>
2021-04-10 22:39:36 +0600

    Commit

--- View/RegVendor.php
+++ View/RegVendor.php
@@ -3,48 +3,50 @@ include('../Control/validvendor.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 </head>
+
 <body>
-<h1>Vendor Registation Form</h1>
-<?php echo "$msg"; ?>
-<form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
+  <h1>Vendor Registation Form</h1>
+  <?php echo "$msg"; ?>
+  <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
     <table>
-    <tr>
-      <td>
-         Company name:
-      </td>
-      <td>
-        <input type="text" name="name">
-      </td>
-    </tr>
+      <tr>
+        <td>
+          Company name:
+        </td>
+        <td>
+          <input type="text" id="cname" name="name">
+        </td>
+      </tr>
 
       <tr>
         <td>
-         Email:
+          Email:
         </td>
         <td>
-          <input type="text" name="email">
-        </td>
-      </tr>
-      <tr>
-        <td>
-        Phone No:
-        </td>
-        <td>
-          <input type="text" name="phone">
+          <input type="text" id="email" name="email">
         </td>
       </tr>
       <tr>
         <td>
-        Trade license No:
+          Phone No:
         </td>
         <td>
-          <input type="text" name="tradel">
+          <input type="text" id="phone" name="phone">
+        </td>
+      </tr>
+      <tr>
+        <td>
+          Trade license No:
+        </td>
+        <td>
+          <input type="text" id="tradel" name="tradel">
         </td>
       </tr>
       <tr>
@@ -52,37 +54,38 @@ include('../Control/validvendor.php');
           Password:
         </td>
         <td>
-          <input type="password" name="pass">
+          <input type="password" id="pass" name="pass">
         </td>
       </tr>
 
       <tr>
         <td>
-         Confirm Password:
+          Confirm Password:
         </td>
         <td>
-          <input type="password" name="cnpass">
+          <input type="password" id="cpass" name="cnpass">
         </td>
       </tr>
 
       <tr>
-          <td>
-              Address:
-          </td>
-          <td>
-              <textarea id="address" name="address" rows="3" cols="30"></textarea>
-          </td>
+        <td>
+          Address:
+        </td>
+        <td>
+          <textarea id="address" name="address" rows="3" cols="30"></textarea>
+        </td>
       </tr>
 
-    <tr>
-      <td>
-        <input type="submit" value="Submit">
-        <input type="reset" value="Reset">
-      </td>
-    </tr>
+      <tr>
+        <td>
+          <input type="submit" value="Submit">
+          <input type="reset" value="Reset">
+        </td>
+      </tr>
 
 
     </table>
   </form>
 </body>
+
 </html>
