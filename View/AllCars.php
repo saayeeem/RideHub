7193a4a7dc4eb3cfd01445a79ad82a0fd5b3d8eb
saayeeem <chysayem9@gmail.com>
2021-04-17 17:58:26 +0600

    Admin page is done except update and request

--- View/AllCars.php
+++ View/AllCars.php
@@ -17,22 +17,23 @@
 
 
     <nav>
-        <a href="CustomerHome.php">Home</a> |
-        <a href="CustomerProfile.php">My Profile</a> |
+        <a href="AdminHome.php">Home</a> |
+        <a href="AdminProfile.php">My Profile</a> |
         <a href="logout.php">Log Out</a>
     </nav>
-    <h1>
-        Your Cars Available
-    </h1>
+
     <!-- main -->
     <section class="pad-70 right">
         <div class="container">
+            <h1>
+                Your Cars Available
+            </h1>
             <?php
             session_start();
             include('../model/db.php');
             $connection = new db();
             $conobj = $connection->OpenCon();
-            $userQuery = $connection->ShowAvailable($conobj, "Car", "Yes");
+            $userQuery  = $connection->Show($conobj, "Car");
 
             if ($userQuery->num_rows > 0) {
 
@@ -44,7 +45,8 @@
                     echo "<table><tr><th>Car Name</th><th>Model</th><th>Sit Count</th><th>Availability</th><th>Action</th></tr>";
                     // output data of each row
                     while ($row = $userQuery->fetch_assoc()) {
-                        echo "<tr><td>" . $row["carname"] . "</td><td>" . $row["carmodel"] . "</td><td>" . $row["sitcount"] . "</td><td>" . $row["availability"] . "</td><td>" . '<a href="AdminHome.php">Request</a>' . "</td></tr>";
+                        echo "<tr><td>" . $row["carname"] . "</td><td>" . $row["carmodel"] . "</td><td>" . $row["sitcount"] . "</td><td>" . $row["availability"] . "</td><td>"
+                            . '<a href="UpdateVendor.php">Edit </a>/' . '<a href="AdminHome.php">Delete </a>' . "</td></tr>";
                     }
                     echo "</table>";
                 }
--- View/AllDriver.php
+++ View/AllDriver.php
@@ -1,12 +1,3 @@
-
-<?php
-session_start();
-if(empty($_SESSION["email"]))
-{
-header("Location: ../control/login.php"); // Redirecting To Home Page
-}
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -14,51 +5,94 @@ header("Location: ../control/login.php"); // Redirecting To Home Page
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="../css/mycss.css">
+    <title>All Drivers</title>
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,900&display=swap"
+        rel="stylesheet">
+
+
 </head>
 
 <body>
-    <nav>
-        <a href="AdminHome.php">Home</a> |
 
+    <div class="header">
+        <h1>Welcome To RideHub</h1>
+        <h2>All Driver Profile</h2>
+    </div>
+
+    <nav>
+
+        <a href="AdminHome.php">Home</a> |
+        <a href="AdminProfile.php">My Profile</a> |
+        <a href="logout.php">Log Out</a>
     </nav>
     <p> <img src="Pictures/driver1.jpg" alt="Home">
     </p>
 
-    <?php
-    include('../Control/validdriver.php');
+    <!-- main -->
+    <section class="pad-70 right">
+        <div class="container">
+            <?php
+            include('../Control/validdriver.php');
 
-    echo "Driver Profile" . "<br>";
+            $connection = new db();
+            $conobj = $connection->OpenCon();
+            // $connection->ShowAll($conobj, "customer", $email);
 
-    $connection = new db();
-    $conobj = $connection->OpenCon();
-    // $connection->ShowAll($conobj, "customer", $email);
+            $userQuery = $connection->Show($conobj, "driver");
 
-    $userQuery = $connection->Show($conobj, "driver");
+            if ($userQuery->num_rows > 0) {
+                echo "<table><tr><th>Name</th><th>Email</th><th>Address</th><th>Phone</th><th>Action</th></tr>";
+                // output data of each row
+                while ($row = $userQuery->fetch_assoc()) {
+                    echo "<tr><td>";
+                    echo (htmlentities($row['name']));
+                    echo ("</td><td>");
+                    echo (htmlentities($row['email']));
+                    echo ("</td><td>");
+                    echo (htmlentities($row['address']));
+                    echo ("</td><td>");
+                    echo (htmlentities($row['phone']));
+                    echo ("</td><td>");
+                    echo ('<a href="AdminHome.php">' . 'Edit</a> / ');
+                    echo ('<a href="AdminHome.php">' . 'Delete</a> ');
+                    echo ("</td></tr>\n");
+                }
+                echo "</table>";
+            } else {
+                echo "0 results";
+            }
+            $connection->CloseCon($conobj);
+            ?>
 
-    if ($userQuery->num_rows > 0) {
-        echo "<table><tr><th>Name</th><th>Email</th><th>Address</th><th>Phone</th><th>Action</th></tr>";
-        // output data of each row
-        while ($row = $userQuery->fetch_assoc()) {
-            echo "<tr><td>";
-            echo (htmlentities($row['name']));
-            echo ("</td><td>");
-            echo (htmlentities($row['email']));
-            echo ("</td><td>");
-            echo (htmlentities($row['address']));
-            echo ("</td><td>");
-            echo (htmlentities($row['phone']));
-            echo ("</td><td>");
-            echo ('<a href="caredit.php?user_id=' . $row['did'] . '">Edit</a> / ');
-            echo ('<a href="cardelete.php?user_id=' . $row['did'] . '">Delete</a>');
-            echo ("</td></tr>\n");
-        }
-        echo "</table>";
-    } else {
-        echo "0 results";
-    }
-    $connection->CloseCon($conobj);
-    ?>
+        </div>
+    </section>
+    <!-- main -->
+    <!-- footer -->
+    <footer>
+        <div class="container footer-wrap">
+            <div class="footer-left">
+                <ul class="footer-menu">
+                    <li><a href="">Terms and Conditions</a></li>
+                    <li><a href="">Privacy</a></li>
+                </ul>
+
+            </div>
+            <div class="footer-right">
+                <ul class="footer-menu">
+                    <li><a href="">Follow</a></li>
+                    <li><a href=""><i class="fab fa-facebook"></i></a></li>
+                    <li><a href=""><i class="fab fa-twitter"></i></a></li>
+                    <li><a href=""><i class="fab fa-instagram"></i></a></li>
+
+                </ul>
+            </div>
+        </div>
+
+
+    </footer>
+    <!-- footer -->
+    <script src="https://kit.fontawesome.com/2065a5e896.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
