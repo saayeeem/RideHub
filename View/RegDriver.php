9e81c5028b983e4bff823b1fea8ff7795acfaf4f
Kazishayem <kazishayem1234@gmail.com>
2021-04-19 05:28:31 +0600

    update reg driver

--- View/RegDriver.php
+++ View/RegDriver.php
@@ -1,134 +1,94 @@
-
-<?php
-session_start();
-if(empty($_SESSION["email"]))
-{
-header("Location: ../control/login.php"); // Redirecting To Home Page
-}
-
-?>
 <?php
 include('../Control/validdriver.php');
 ?>
 
-<html>
+<!DOCTYPE html>
+<html lang="en">
 
-<body>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../css/mycss.css">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,900&display=swap"
+        rel="stylesheet">
+    <script src="../js/DriverValidation.js"></script>
+    <title>Driver Registration</title>
+</head>
+
+
 
     <body>
-        <?php
-        include('../View/MenuFooter.php');
+    <div class="header">
+        <h1>Welcome To RideHub</h1>
+        <h2>Driver Registration</h2>
+    </div>
 
 
-        ?>
         <nav class="topnav">
             <a href="DriverHome.php">Home</a> |
             <a href="DriverProfile.php">My Profile</a> |
             <a href="logout.php">Log Out</a>
         </nav>
 
-        <br>
-        <div id="page-container">
-            <div id="content-wrap">
-                <div class="bg" id="center">
-                    <h1>
-                        <script src="../js/DriverValidation.js"></script>
+ <section class="pad-70 form-pos">
+  <div class="container">
+            <h1>Driver Registration Form</h1>
+            <?php
+            // Note triple not equals and think how badly double
+            // not equals would work here...
+            
+            echo "$msg";
+            ?>
+            <p id="error">
+            </p>
+            <form action=" <?php echo $_SERVER["PHP_SELF"]; ?>" onsubmit="return validateUserForm()" method="post">
+                <div class="form-row">
+                    <div class="form-group">
+                       Driver Name:
+                        <input type="text" name="name" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        Email:
+                        <input type="text" name="email" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        Phone No:
+                        <input type="text" name="phone" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        Driving license No:
+                        <input type="text" name="driving1" class="form-control">
+                    </div>
 
-                        <center>Driver Registration Form</center>
-                    </h1>
-                    <img src="Pictures/driver.jpg" alt="Home" class="center">
+                    <div class="form-group">
+                        Password:
+                        <input type="password" name="pass" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        Confirm Password:
+                        <input type="password" name="cpass" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        Date of Birth:
+                        <input type="date" name="birthday" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        Address:
+                        <input type="text" name="address" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" value="Update" name="update" class="btn btn-lg btn-primary btn-submit">
+                    </div>
+                    <div class="form-group">
+                        <input type="reset" value="reset" name="reset" class="btn btn-lg btn-primary btn-submit">
+                    </div>
+                </div>
 
-                    <?php echo "$msg"; ?>
-                    <p id="error">
+            </form>
+        </div>
+    </section>
 
-                    <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" onsubmit="return validateUserForm()" method="post">
-                        <table>
-
-                            <tr>
-                                <td>
-                                    Driver Name:
-                                </td>
-                                <td>
-                                    <input type="text" id="name" name="name">
-
-                                </td>
-                            </tr>
-
-                            <tr>
-                                <td>
-                                    Email:
-                                </td>
-                                <td>
-                                    <input type="text" id="email" name="email">
-
-                                </td>
-                            </tr>
-
-                            <tr>
-                                <td>
-                                    Phone No:
-                                </td>
-                                <td>
-                                    <input type="text" id="phone" name="phone">
-
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    Driving license No:
-                                </td>
-                                <td>
-                                    <input type="text" id="dlicense" name="drivingl">
-
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    Password:
-                                </td>
-                                <td>
-                                    <input type="password" id="pass" name="pass">
-                                </td>
-                            </tr>
-
-                            <tr>
-                                <td>
-                                    Confirm Password:
-                                </td>
-                                <td>
-                                    <input type="password" id="cpass" name="cnpass">
-                                </td>
-                            </tr>
-
-                            <tr>
-                                <td>
-                                    Date of Birth:
-                                </td>
-                                <td>
-                                    <input type="date" id="birthday" name="dob">
-                                </td>
-                            </tr>
-
-                            <tr>
-                                <td>
-                                    Address:
-                                </td>
-                                <td>
-                                    <textarea id="address" name="address" rows="3" cols="30"></textarea>
-                                </td>
-                            </tr>
-
-                            <tr>
-                                <td>
-                                    <input type="submit" value="Submit">
-                                    <input type="reset" value="Reset">
-
-
-                                </td>
-                            </tr>
-
-
-                        </table>
                     </form>
                 </div>
             </div>
