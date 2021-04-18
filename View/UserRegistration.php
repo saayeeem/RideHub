1e984ddfad0341ad01baeb4d0d396fc308a65332
saayeeem <chysayem9@gmail.com>
2021-04-18 22:19:38 +0600

    Updated User Form CSS

--- View/UserRegistration.php
+++ View/UserRegistration.php
@@ -27,134 +27,54 @@ include('../Control/UserValidation.php');
     <h1>Customer Registration Form</h1>
     <?php echo "$msg"; ?>
     <p id="error">
+    </p>
 
-    <form action=" <?php echo $_SERVER["PHP_SELF"]; ?>" onsubmit="return validateUserForm()" method="post">
     <section class="pad-70">
-
-<div class="container">
-    <form action='' method='post'>
-        <div class="form-row">
-            <div class="form-group">
-              Name:
-                <input type="text" name="name" class="form-control">
-            </div>
-            <div class="form-group">
-                Email:
-                <input type="text" name="email" class="form-control">
-            </div>
-            <div class="form-group">
-                Phone No:
-                <input type="text" name="phone"  class="form-control">
-            </div>
-            
-            <div class="form-group">
-                Password:
-                <input type="password" name="pass" class="form-control">
-            </div>
-            <div class="form-group">
-                Confirm Password:
-                <input type="password" name="cpass" class="form-control">
-            </div>
-            <div class="form-group">
-                Date of Birth:
-                <input type="date" name="birthday" class="form-control">
-            </div>
-            <div class="form-group">
-                Address:
-                <input type="text" name="address" class="form-control">
-            </div>
-            <div class="form-group">
-                <input type="submit" value="Update" name="update" class="btn btn-lg btn-primary btn-submit">
-            </div>
-            <div class="form-group">
-                <input type="reset" value="reset" name="reset" class="btn btn-lg btn-primary btn-submit">
-            </div>
-        </div>
-
-    </form>
-</div>
-</section>
-    </form>
-
-    <section class="pad-70 right">
         <div class="container">
             <form action=" <?php echo $_SERVER["PHP_SELF"]; ?>" onsubmit="return validateUserForm()" method="post">
-                <table>
+                <div class="form-row">
+                    <div class="form-group">
+                        Name:
+                        <input type="text" name="name" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        Email:
+                        <input type="text" name="email" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        Phone No:
+                        <input type="text" name="phone" class="form-control">
+                    </div>
 
-                    <tr>
-                        <td>
-                            Name:
-                        </td>
-                        <td>
-                            <input type="text" id="name" name="name">
-                        </td>
-                    </tr>
+                    <div class="form-group">
+                        Password:
+                        <input type="password" name="pass" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        Confirm Password:
+                        <input type="password" name="cpass" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        Date of Birth:
+                        <input type="date" name="birthday" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        Address:
+                        <input type="text" name="address" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" value="Update" name="update" class="btn btn-lg btn-primary btn-submit">
+                    </div>
+                    <div class="form-group">
+                        <input type="reset" value="reset" name="reset" class="btn btn-lg btn-primary btn-submit">
+                    </div>
+                </div>
 
-                    <tr>
-                        <td>
-                            Email:
-                        </td>
-                        <td>
-                            <input type="text" id="email" name="email">
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>
-                            Phone No:
-                        </td>
-                        <td>
-                            <input type="text" id="phone" name="phone">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Password:
-                        </td>
-                        <td>
-                            <input type="password" id="pass" name="pass">
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>
-                            Confirm Password:
-                        </td>
-                        <td>
-                            <input type="password" id="cpass" name="cpass">
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>
-                            Date of Birth:
-                        </td>
-                        <td>
-                            <input type="date" id="birthday" name="birthday">
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>
-                            Address:
-                        </td>
-                        <td>
-                            <textarea id="address" name="address" rows="3" cols="30"></textarea>
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>
-                            <input type="submit" value="Submit">
-                            <input type="reset" value="Reset">
-                        </td>
-                    </tr>
-
-
-                </table>
             </form>
         </div>
     </section>
+    </form>
+
 
 </body>
 
