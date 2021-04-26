7da7fedb6c6f1751881a8f909f4b13ae03127f53
Kazishayem <kazishayem1234@gmail.com>
2021-04-26 17:50:12 +0600

    Update DriverHome.php

--- View/DriverHome.php
+++ View/DriverHome.php
@@ -64,7 +64,7 @@ if (!isset($_SESSION['email'])) {
             <div class="container">
                 <div class="row">
 
-                    <div class="post post-right">
+                    <div class="post post-center">
                         <img src="Pictures/img4.png" alt="show car">
                         <div class="tag"> <a href="ShowCarDriver.php">Requested Car By User</a></div>
                     </div>
